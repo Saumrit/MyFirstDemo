e55bcb24459e2416b81ddeba2e3a1f9d5899110c
Saumrit <44732905+Saumrit@users.noreply.github.com>
2021-12-06 13:31:30 +0530

    Cloned from GIThub

---
+++ MyfirstDemo/src/main/java/com/saumrit/MyfirstDemo/MyfirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.saumrit.MyfirstDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.saumrit")
+public class MyfirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstDemoApplication.class, args);
+	}
+
+}
---
+++ MyfirstDemo/src/test/java/com/saumrit/MyfirstDemo/MyfirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumrit.MyfirstDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
